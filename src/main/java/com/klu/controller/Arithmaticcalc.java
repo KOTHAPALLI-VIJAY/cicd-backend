2ef5aeb3a15fb52412157c3b17cece0ffa1e5264
KOTHAPALLI-VIJAY <vijaykothapalli26@gmail.com>
2025-08-26 12:00:16 +0530

    cm1

--- src/main/java/com/klu/controller/Arithmaticcalc.java
+++ src/main/java/com/klu/controller/Arithmaticcalc.java
@@ -1,18 +1,30 @@
 package com.klu.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
-@CrossOrigin("*")
+@RequestMapping("/")   // base path
+@CrossOrigin(origins ="http://localhost:9090/")
 public class Arithmaticcalc {
-	@GetMapping("add/{A}/{B}")
-	public String add(@PathVariable("A") int a,@PathVariable("B")int b)
-	{
-		return "Addition = "+(a+b);
-	}
+
+    @GetMapping("sub/{A}/{B}")
+    public String sub(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Subtraction = " + (a - b);
+    }
+
+    @GetMapping("add/{A}/{B}")
+    public String add(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Addition = " + (a + b);
+    }
+
+    @GetMapping("mul/{A}/{B}")
+    public String mul(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Multiplication = " + (a * b);
+    }
+
+    @GetMapping("div/{A}/{B}")
+    public String div(@PathVariable("A") int a, @PathVariable("B") int b) {
+        if (b == 0) return "Division by zero not allowed!";
+        return "Division = " + (a / b);
+    }
 }
